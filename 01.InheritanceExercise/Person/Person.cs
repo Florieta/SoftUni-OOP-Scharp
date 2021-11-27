a902e8ed06b6d79da8ed0559cc4b393ac8cafa5a
Florieta <florieta.pencheva@gmail.com>
2021-11-27 22:53:24 +0200

    Added some problem solutions about Inheritance Exercise

---
+++ 01.InheritanceExercise/Person/Person.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Person
+{
+    class Person
+    {
+        private int age;
+        public string Name { get; set; }
+
+        public virtual int Age
+        {
+            get
+            {
+                return this.age;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Age cannot be negative.");
+                }
+
+                this.age = value;
+            }
+        }
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {this.Name}, Age: {this.Age}";
+        }
+    }
+}
---
+++ 01.InheritanceExercise/Zoo/StartUp.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Zoo
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
