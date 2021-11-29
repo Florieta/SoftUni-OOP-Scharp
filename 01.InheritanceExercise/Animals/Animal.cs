00f2f1d6c5a40e880138d8ff32b7eb91e978fb3d
Florieta <florieta.pencheva@gmail.com>
2021-11-29 15:02:53 +0200

    Added some problem solutions about Inheritance

---
+++ 01.InheritanceExercise/Animals/Animal.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals
+{
+    public class Animal
+    {
+        public Animal(string name, int age, string gender)
+        {
+            Name = name;
+            Age = age;
+            Gender = gender;
+        }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public string Gender { get; set; }
+
+        public string Sound { get; set; }
+        public string ProduceSound()
+        => Sound;
+
+        public override string ToString()
+        {
+            return $"{this.GetType().Name}\r\n" +
+                $"{Name} {Age} {Gender}\r\n" +
+                $"{ProduceSound()}";
+        }
+    }
+}
---
+++ 01.InheritanceExercise/Restaurant/Cake .cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+{
+    public class Cake : Dessert
+    {
+       
+        private const double DefaultGrams = 250;
+
+        private const double DefaultCalories = 1000;
+
+        private const decimal CakePrice = 5M;
+
+        public Cake(string name) 
+            : base(name, CakePrice, DefaultGrams, DefaultCalories)
+        {
+            
+        }
+    }
+}
