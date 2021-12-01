175fdfd2dbc578b6b920b06b01fcd174cc3f6ae2
Florieta <florieta.pencheva@gmail.com>
2021-12-01 21:40:07 +0200

    Added some problem solutions about Encapsulation

---
+++ 02.EncapsultionLab/Salary/Person.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonsInfo
+{
+    public class Person
+    {
+        public Person(string firstName, string lastName, int age, decimal salary)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            Salary = salary;
+        }
+
+        public string FirstName { get; private set; }
+
+        public string LastName { get; private set; }
+
+        public int Age { get; private set; }
+
+        public decimal Salary { get; private set; }
+        public void IncreaseSalary(decimal percentage)
+        {
+            decimal increase = percentage;
+            if (Age < 30)
+            {
+                increase = percentage / 2;
+            }
+            Salary += Salary * increase / 100;
+        }
+
+
+        public override string ToString()
+        {
+            return $"{this.FirstName} {this.LastName} receives {Salary:F2} leva.";
+        }
+    }
+}
