53b7cd13ea8bd744004e4034d654534ffea4dd8a
Florieta <florieta.pencheva@gmail.com>
2021-12-16 13:40:58 +0200

    Added some problem solutions about Exception Handling

---
+++ 05.ExceptionHandling/FixingVol/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace FixingVol
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber, secondNumber;
+            byte result;
+
+            firstNumber = 30;
+            secondNumber = 60;
+
+            try
+            {
+                result = Convert.ToByte(firstNumber * secondNumber);
+                Console.WriteLine($"{firstNumber} x {secondNumber} = {result}");
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 05.ExceptionHandling/ValidPerson/InvalidPersonNameException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ValidPerson
+{
+    public class InvalidPersonNameException : Exception
+    {
+        public InvalidPersonNameException(string message)
+           : base(message)
+        {
+        }
+    }
+}
